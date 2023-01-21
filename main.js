7f26a80ed33f5d390798a29b0ec19504c54ef32c
masaue <masayuki.uegaki@gmail.com>
2023-01-21 15:34:25 +0900

    GPIO-Button

--- main.js
+++ main.js
@@ -1,18 +1,37 @@
+// タクトスイッチでLEDの点灯を行うチュートリアル
 const { requestGPIOAccess } = require("node-web-gpio");
-const sleep = require("util").promisify(setTimeout);
 
-async function blink() {
-  const gpioAccess = await requestGPIOAccess();
-  const port = gpioAccess.ports.get(26);
+async function button() {
+    const gpioAccess = await requestGPIOAccess();
 
-  await port.export("out");
+    // LEDの接続されているポートの指定
+    const ledPort = gpioAccess.ports.get(26);
+    await ledPort.export("out");
 
-  for (;;) {
-    await port.write(1);
-    await sleep(1000);
-    await port.write(0);
-    await sleep(1000);
-  }
+    // タクトスイッチの接続されているポートの指定
+    const switchPort = gpioAccess.ports.get(5);
+    await switchPort.export("in");
+
+    // 点灯回数をカウントする引数cnt
+    var cnt = 0;
+
+    switchPort.onchange = function(val) {
+        // ボタンを押すと点灯、離すと消灯する
+        if (cnt > 9 && val.value == 0){
+            console.log("Light : OFF");
+            process.exit(1);
+        }
+
+        if(val.value == 1){
+            ledPort.write(0);
+            console.log("Light : OFF");
+            cnt = cnt + 1;
+        } else if(val.value == 0){
+            ledPort.write(1);
+            console.log("Light : ON");
+            cnt = cnt + 1;
+        }
+    };
 }
 
-blink();
+button();
